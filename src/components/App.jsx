b4e6e9b0e6fc1c0d56e88d2a0eeb9a3e6b2338ec
ozgeerkskn <105421946+ozgeerkskn@users.noreply.github.com>
2023-12-28 18:25:31 +0300

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeading] = useState("");
+  const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  function handleClick(event) {
+    setHeading(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
